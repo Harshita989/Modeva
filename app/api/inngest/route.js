fb1436955a1a4d58ab8761996ec8bbd2f15c9c04
Harshita <harshitakhandoli@gmail.com>
2025-11-12 12:13:49 +0530

    updation

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "@config/inngest";
-import { syncUserCreation, syncUserUpdation } from "@/config/inngest";
+import { syncUserCreation, syncUserUpdation ,syncUserDeletion} from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
